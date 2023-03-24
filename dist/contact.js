c730a9b1d80dbabed444f9cef11612c3cf852271
Taylor Todd <ttodd1031@gmail.com>
2023-03-24 15:31:49 -0500

    added tabbed browsing

---
+++ dist/contact.js
@@ -0,0 +1,34 @@
+function loadContact() {
+    //reset page
+    const mainContent = document.getElementById("mainContent");
+    mainContent.innerHTML = ``;
+
+    // main section
+    const contactSec = document.createElement("div");
+    contactSec.setAttribute('id', 'contactSec')
+
+    //header
+    const contactUs = document.createElement("h1");
+    contactUs.setAttribute("id", "contactHeader");
+    contactUs.innerText = "Order Now!"
+    mainContent.append(contactSec);
+    contactSec.append(contactUs);
+
+    const instaLink = document.createElement("a");
+    instaLink.setAttribute("href", "https://www.instagram.com/chefdomllc/?hl=en");
+    const instaLogo = document.createElement("img");
+    instaLogo.setAttribute("src", "/instagram.svg");
+    instaLogo.setAttribute("height", "200px");
+    contactSec.append(instaLogo);
+    instaLink.appendChild(instaLogo);
+    contactSec.appendChild(instaLink);
+
+    const emailHeader = document.createElement("h1");
+    emailHeader.innerText = "Business Inquiries: ChefDomLLC21@gmail.com";
+    contactSec.append(emailHeader);
+
+
+
+};
+
+export default loadContact;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+import loadContact from "../dist/contact";
 import loadPage from "../dist/pageload";
+
 document.addEventListener("DOMContentLoaded", (e) => {
+    // initialize
     loadPage();
-}) 
-console.log("test");
+
+    // find buttons
+    const homeBtn = document.getElementById("home");
+    const menuBtn = document.getElementById("menu");
+    const contactBtn = document.getElementById("contact");
+
+  
+
+    document.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target == homeBtn) {
+            loadPage();
+        } else if (e.target == contactBtn) {
+            loadContact();
+        }
+    });
+});
