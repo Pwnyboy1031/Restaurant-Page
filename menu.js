4d01fe864411fbcbca1c2352b85dc02ecab00d4a
Taylor Todd <ttodd1031@gmail.com>
2023-03-24 17:20:06 -0500

    added menu card mockup

--- menu.js
+++ menu.js
@@ -12,12 +12,43 @@ function loadMenu() {
     mainContent.append(menuWrap);
     menuWrap.appendChild(menuItem1);
 
+    const menuItem2 = document.createElement("div");
+    menuItem2.setAttribute("class", "menu");
+    mainContent.append(menuWrap);
+    menuWrap.appendChild(menuItem2);
+
+    const menuItem3 = document.createElement("div");
+    menuItem3.setAttribute("class", "menu");
+    mainContent.append(menuWrap);
+    menuWrap.appendChild(menuItem3);
+
     //menu item 1
     const menuImage1 = document.createElement("img");
     menuImage1.setAttribute("src", "/pizzarolls.png");
     menuItem1.appendChild(menuImage1);
     const menuCaption1 = document.createElement("div");
     menuCaption1.setAttribute("class", "menuCaption");
+    menuItem1.appendChild(menuCaption1);
+    menuCaption1.innerText = "Pizza Rolls"
+
+    //menu item 2
+    const menuImage2 = document.createElement("img");
+    menuImage2.setAttribute("src", "/chkwaffles.png");
+    menuItem2.appendChild(menuImage2);
+    const menuCaption2 = document.createElement("div");
+    menuCaption2.setAttribute("class", "menuCaption");
+    menuItem2.appendChild(menuCaption2);
+    menuCaption2.innerText = "Chicken and Waffles"
+
+    //menu item 3
+    const menuImage3 = document.createElement("img");
+    menuImage3.setAttribute("src", "/cheesecake.png");
+    menuItem3.appendChild(menuImage3);
+    const menuCaption3 = document.createElement("div");
+    menuCaption3.setAttribute("class", "menuCaption");
+    menuItem3.appendChild(menuCaption3);
+    menuCaption3.innerText = "Cheesecake"
+
 
 
 
